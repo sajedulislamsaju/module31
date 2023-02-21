75c9999c53a736c25bd2c48c8107e1e84a9d84ac
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-02-21 18:06:44 +0600

    this is the secend commit

--- difolt.js
+++ difolt.js
@@ -1,7 +1,17 @@
 function add(first , secend ){
     const sum = first + secend ;
-    console.log(sum)
+    // console.log(sum)
 }
 
 
-add(10, 10)
+// add(10, 10)
+
+
+function add(first , secend){
+    const friend = first + ' ' + secend;
+    return friend;
+
+}
+
+const total = (add ('soikot', 'ali'))
+console.log(total)
