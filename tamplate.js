1fef883e3b0ecd8c2fca11335dd44d66f49f58d2
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-02-21 20:39:49 +0600

    tamplate.js commit

---
+++ tamplate.js
@@ -0,0 +1,22 @@
+// console.log(47+834)
+
+const Rname = 'Sajjad ';
+const lastName = 'Hosain';
+const fullName = Rname + lastName;
+// console.log(fullName);
+
+
+const story = 'this is the first line \n'+ 
+'this is the secend line \n '+ 
+'this is the third lone \n'+
+'this is the 4th line '
+
+// console.log(story)
+
+
+const reStory = `this is the first line 
+this is the secend line 
+this  is the third line 
+this is the 4th line `
+
+// console.log(reStory)
